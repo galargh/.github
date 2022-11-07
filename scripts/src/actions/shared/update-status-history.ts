d3061a425cd373878e4f0320b344671955aef3c8
Piotr Galar <piotr.galar@gmail.com>
2022-11-07 13:59:27 +0100

    Status history updates (#32)
    * feat: switch status history update impl
    
    * fix: ts formatting
    
    * fix: graphql queries
    
    * fix: optionally retrieve field from value
    
    * feat: print out item title on status history update
    
    * chore: disable dry run on status updates
    
    * fix: field updates
    
    * fix: zero pad date
    
    * fix: status history updates
    
    * feat: enable new status history updates on other projects

--- scripts/src/actions/shared/update-status-history.ts
+++ scripts/src/actions/shared/update-status-history.ts
@@ -3,18 +3,21 @@ import * as core from '@actions/core'
 import assert from 'assert'
 import { GraphQlResponse } from '@octokit/graphql/dist-types/types'
 
-async function updateProjectV2ItemFieldValue(projectId: string, itemId: string, fieldId: string, value: string): GraphQlResponse<unknown> {
+async function updateProjectV2ItemFieldValue(
+  projectId: string,
+  itemId: string,
+  fieldId: string,
+  value: any
+): GraphQlResponse<unknown> {
   const github = await GitHub.getGitHub()
   return github.client.graphql(
-    `mutation($projectId: ID!, $itemId: ID!, $fieldId: ID!, $value: String!) {
+    `mutation($projectId: ID!, $itemId: ID!, $fieldId: ID!, $value: ProjectV2FieldValue!) {
       updateProjectV2ItemFieldValue(
         input: {
           projectId: $projectId
           itemId: $itemId
           fieldId: $fieldId
-          value: {
-            text: $value
-          }
+          value: $value
         }
       ) {
         projectV2Item {
@@ -37,8 +40,8 @@ export async function updateStatusHistory(
   dryRun: boolean
 ) {
   const date = new Date()
-  const timestamp = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}`
-  const simpleDate = `${date.getUTCDate()}/${date.getUTCMonth()}/${date.getUTCFullYear()}`
+  const datetime = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}`
+  const dateonly = `${date.getUTCFullYear()}-${('0' + date.getUTCMonth()).slice(-2)}-${('0' + date.getUTCDate()).slice(-2)}`
   const github = await GitHub.getGitHub()
 
   core.info(`Searching for project: ${org}/${projectNumber}`)
@@ -46,26 +49,38 @@ export async function updateStatusHistory(
   const {
     organization: { projectV2: project }
   } = (await github.client.graphql.paginate(
-    `query($login: String!, $number: Int!) {
+    `query($login: String!, $number: Int!, $cursor: String) {
       organization(login: $login) {
         projectV2(number: $number) {
           id
           fields(first: 100) {
             nodes {
-              id
-              name
-              settings
+              ... on Node {
+                id
+              }
+              ... on ProjectV2FieldCommon {
+                name
+              }
             }
           }
           items(first: 100, after: $cursor) {
             nodes {
               id
-              title
+              isArchived
               fieldValues(first: 100) {
                 nodes {
-                  value
-                  projectField {
-                    id
+                  ... on ProjectV2ItemFieldTextValue {
+                    text
+                  }
+                  ... on ProjectV2ItemFieldSingleSelectValue {
+                    name
+                  }
+                  ... on ProjectV2ItemFieldValueCommon {
+                    field {
+                      ... on Node {
+                        id
+                      }
+                    }
                   }
                 }
               }
@@ -86,40 +101,87 @@ export async function updateStatusHistory(
   core.info(`Found project: ${project.id}`)
 
   core.info(`Checking if required fields exist`)
-  const statusField = project.fields.nodes.find((field: any) => field.name === 'Status')
+  const statusField = project.fields.nodes.find(
+    (field: any) => field.name === 'Status'
+  )
   assert(statusField, `Project must have a field named 'Status'`)
-  const statusDateField = project.fields.nodes.find((field: any) => field.name === 'Status Date')
+  const statusDateField = project.fields.nodes.find(
+    (field: any) => field.name === 'Status Date'
+  )
   assert(statusDateField, `Project must have a field named 'Status Date'`)
-  const statusHistoryField = project.fields.nodes.find((field: any) => field.name === 'Status History')
+  const statusHistoryField = project.fields.nodes.find(
+    (field: any) => field.name === 'Status History'
+  )
   assert(statusHistoryField, `Project must have a field named 'Status History'`)
-  const statusTimestampField = project.fields.nodes.find((field: any) => field.name === 'Status Timestamp')
-  assert(statusTimestampField, `Project must have a field named 'Status Timestamp'`)
+  const statusTimestampField = project.fields.nodes.find(
+    (field: any) => field.name === 'Status Timestamp'
+  )
+  assert(
+    statusTimestampField,
+    `Project must have a field named 'Status Timestamp'`
+  )
+  const titleField = project.fields.nodes.find(
+    (field: any) => field.name === 'Title'
+  )
   core.info(`Found all required fields`)
 
   core.info(`Updating status history for all items`)
   for (const item of project.items.nodes) {
-    if (item.title === "You can't see this item") {
-      core.warning(`The following item is inaccessible: ${JSON.stringify(item)}`)
+    const title = item.fieldValues.nodes.find(
+      (fieldValue: any) => fieldValue.field?.id === titleField.id
+    )?.text
+    if (title === "You can't see this item") {
+      core.warning(`Item is inaccessible: ${item.id} (${title})`)
       continue
     }
-    core.debug(`Checking if item needs to be updated: ${item.id} (${item.title})`)
-    const status = item.fieldValues.nodes.find((field: any) => field.projectField.id === statusField.id)?.value
-    const statusHistory = JSON.parse(item.fieldValues.nodes.find((fieldValue: any) => fieldValue.projectField.id === statusHistoryField.id)?.value || '[]')
+    if (item.isArchived) {
+      core.debug(`Item is archived: ${item.id} (${title})`)
+      continue
+    }
+    core.debug(`Checking if item needs to be updated: ${item.id} (${title})`)
+    const status = item.fieldValues.nodes.find(
+      (fieldValue: any) => fieldValue.field?.id === statusField.id
+    )?.name
+    const statusHistory = JSON.parse(
+      item.fieldValues.nodes.find(
+        (fieldValue: any) => fieldValue.field?.id === statusHistoryField.id
+      )?.text || '[]'
+    )
     if (status === statusHistory.at(0)) {
-      core.debug(`Item is up to date: ${item.id} (${item.title})`)
+      core.debug(`Item is up to date: ${item.id} (${title})`)
       continue
     }
     if (dryRun) {
-      core.info(`Would have updated item: ${item.id} (${item.title})`)
+      core.info(`Would have updated item: ${item.id} (${title})`)
       continue
     }
-    core.info(`Updating item: ${item.id} (${item.title})`)
-    const newStatusHistory = [status.value, ...statusHistory.slice(0, 1)]
-    await updateProjectV2ItemFieldValue(project.id, item.id, statusHistoryField.id, JSON.stringify(newStatusHistory))
-    await updateProjectV2ItemFieldValue(project.id, item.id, statusTimestampField.id, timestamp)
-    await updateProjectV2ItemFieldValue(project.id, item.id, statusDateField.id, simpleDate)
-    core.info(`Updated item: ${item.id} (${item.title})`)
-
+    core.info(`Updating item: ${item.id} (${title})`)
+    const newStatusHistory = [status, ...statusHistory.slice(0, 1)]
+    await updateProjectV2ItemFieldValue(
+      project.id,
+      item.id,
+      statusHistoryField.id,
+      {
+        text: JSON.stringify(newStatusHistory)
+      }
+    )
+    await updateProjectV2ItemFieldValue(
+      project.id,
+      item.id,
+      statusTimestampField.id,
+      {
+        text: datetime
+      }
+    )
+    await updateProjectV2ItemFieldValue(
+      project.id,
+      item.id,
+      statusDateField.id,
+      {
+        date: dateonly
+      }
+    )
+    core.info(`Updated item: ${item.id} (${title})`)
   }
   core.info(`Done`)
 }
